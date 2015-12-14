624ac543f99458046d86fd5764d8cf1f482fd8f1
Timo Goebel <timo.goebel@dm.de>
2015-12-14 10:41:18 +0100

    use shortname for validation

--- app/models/concerns/foreman_host_extra_validator/host_extensions.rb
+++ app/models/concerns/foreman_host_extra_validator/host_extensions.rb
@@ -7,8 +7,7 @@ module ForemanHostExtraValidator
 
     def validate_name_by_regex
       return unless validate_name_regex
-      return if name =~ /#{validate_name_regex}/
-      errors.add(:name, _('must match regex /%s/') % validate_name_regex)
+      errors.add(:name, _('must match regex /%s/') % validate_name_regex) unless shortname =~ /#{validate_name_regex}/
     end
 
     def validate_name_regex
