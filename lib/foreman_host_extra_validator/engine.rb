theforeman/foreman_host_extra_validator

commit 4d5d35da8385ea9779ba856e173c683318f15b09

Restore the compatibility with Foreman 3.4

lib/foreman_host_extra_validator/engine.rb
@@ -4,17 +4,19 @@ module ForemanHostExtraValidator
 
     config.autoload_paths += Dir["#{config.root}/app/models/concerns"]
 
-    initializer 'foreman_host_extra_validator.load_default_settings', :before => :load_config_initializers do |_app|
-      require_dependency File.expand_path('../../app/models/setting/foreman_host_extra_validator.rb', __dir__) if begin
-                                                                                                                    Setting.table_exists?
-                                                                                                                  rescue
-                                                                                                                    (false)
-                                                                                                                  end
-    end
-
     initializer 'foreman_host_extra_validator.register_plugin', :before => :finisher_hook do |_app|
       Foreman::Plugin.register :foreman_host_extra_validator do
-        requires_foreman '>= 1.18'
+        requires_foreman '>= 3.0.0'
+
+        settings do
+          category(:host_extra_validator, N_('Host Extra Validator')) do
+            setting('host_name_validation_regex',
+                    description:  N_('Default regex the name of a host is validated against'),
+                    type: :string,
+                    default: '^[a-zA-Z0-9\-_]+$',
+                    full_name: N_('Regular expresssion'))
+          end
+        end
       end
     end
 

app/models/setting/foreman_host_extra_validator.rb
@@ -1,16 +1,0 @@
-class Setting
-  class ForemanHostExtraValidator < ::Setting
-    def self.load_defaults
-      # Check the table exists
-      return unless super
-
-      self.transaction do
-        [
-          self.set('host_name_validation_regex', _('Default regex the name of a host is validated against'), '^[a-zA-Z0-9\-_]+$')
-        ].compact.each { |s| self.create! s.update(:category => 'Setting::ForemanHostExtraValidator') }
-      end
-
-      true
-    end
-  end
-end

db/migrate/20220923181701_fix_extra_validation_settings_category_to_dsl.rb
@@ -1,0 +1,5 @@
+class FixExtraValidationSettingsCategoryToDsl < ActiveRecord::Migration[6.0]
+  def up
+    Setting.where(category: 'Setting::ForemanHostExtraValidator').update_all(category: 'Setting')
+  end
+end

commit 5bc487138740615fcf20c64e3eb80e9924f1b82a

actually load migrations when initializing the engine
Fixes: 4d5d35da8385ea9779ba856e173c683318f15b09

lib/foreman_host_extra_validator/engine.rb
@@ -4,6 +4,13 @@ module ForemanHostExtraValidator
 
     config.autoload_paths += Dir["#{config.root}/app/models/concerns"]
 
+    # Add any db migrations
+    initializer 'foreman_host_extra_validator.load_app_instance_data' do |app|
+      ForemanHostExtraValidator::Engine.paths['db/migrate'].existent.each do |path|
+        app.config.paths['db/migrate'] << path
+      end
+    end
+
     initializer 'foreman_host_extra_validator.register_plugin', :before => :finisher_hook do |_app|
       Foreman::Plugin.register :foreman_host_extra_validator do
         requires_foreman '>= 3.0.0'