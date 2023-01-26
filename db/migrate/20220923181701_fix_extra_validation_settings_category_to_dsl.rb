60efb9f264e3635daf9ec38ac2ab3a8295f0fb69
Leos Stejskal <github@stejskalleos.cz>
2023-01-26 14:32:04 +0100

    Update 20220923181701_fix_extra_validation_settings_category_to_dsl.rb

--- db/migrate/20220923181701_fix_extra_validation_settings_category_to_dsl.rb
+++ db/migrate/20220923181701_fix_extra_validation_settings_category_to_dsl.rb
@@ -1,5 +1,5 @@
 class FixExtraValidationSettingsCategoryToDsl < ActiveRecord::Migration[6.0]
   def up
-    Setting.where(category: 'Setting::ForemanHostExtraValidator').update_all(category: 'Setting')
+    Setting.where(category: 'Setting::ForemanHostExtraValidator').update_all(category: 'Setting') if column_exists?(:settings, :category)
   end
 end
